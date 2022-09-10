3cef91f8565ee0dede062e0f9afd35ac118071ae
afeiship <1290657123@qq.com>
2022-09-10 10:53:37 +0800

    chore: add docs to typeing

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,14 @@
 type QsResponse = Record<string, any>;
 
 interface NxStatic {
+  /**
+   * Get response from a query string.
+   */
   qs(): QsResponse
+
+  /**
+   * Get response from input string.
+   * @param path Path to the query string or a query string like.
+   */
   qs(path: string): QsResponse
 }
