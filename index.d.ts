37bed42b17a9e4b2ba89595d27c364256b3d2c4c
afeiship <1290657123@qq.com>
2022-12-01 10:14:26 +0800

    chore: simplfy d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,7 @@
-type QsResponse = Record<string, any>;
-
 interface NxStatic {
-  /**
-   * Get response from a query string.
-   */
-  qs(): QsResponse
-
   /**
    * Get response from input string.
    * @param path Path to the query string or a query string like.
    */
-  qs(path: string): QsResponse
+  qs(path?: string): Record<string, any>
 }
