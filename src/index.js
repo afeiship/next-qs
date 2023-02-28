7ed6433706fe7d4fda530a6ba2304e6f05c85e89
afeiship <1290657123@qq.com>
2023-02-28 13:35:01 +0800

    feat: can used on bun

--- src/index.js
+++ src/index.js
@@ -1,40 +1,38 @@
-(function () {
-  var global = global || this || window || Function('return this')();
-  var nx = global.nx || require('@jswork/next');
-  var CHAR_AND = '&';
-  var CHAR_EQUAL = '=';
+var global = global || this || window || Function('return this')();
+var nx = global.nx || require('@jswork/next');
+var CHAR_AND = '&';
+var CHAR_EQUAL = '=';
 
-  nx.qs = function (inUrl) {
-    var result = {};
-    var query =
-      inUrl == null ? location.search.substring(1) : inUrl.substring(inUrl.indexOf('?') + 1);
+nx.qs = function (inUrl) {
+  var result = {};
+  var query =
+    inUrl == null ? location.search.substring(1) : inUrl.substring(inUrl.indexOf('?') + 1);
 
-    var params = query.split(CHAR_AND);
-    var arr, pair, key, value;
-    params.forEach(function (param) {
-      pair = param.split(CHAR_EQUAL);
-      key = pair[0];
-      value = pair[1];
-      if (value) {
-        var _value = decodeURIComponent(value);
-        switch (typeof result[key]) {
-          case 'undefined':
-            result[key] = _value;
-            break;
-          case 'string':
-            arr = [result[key], _value];
-            result[key] = arr;
-            break;
-          default:
-            result[key].push(_value);
-        }
+  var params = query.split(CHAR_AND);
+  var arr, pair, key, value;
+  params.forEach(function (param) {
+    pair = param.split(CHAR_EQUAL);
+    key = pair[0];
+    value = pair[1];
+    if (value) {
+      var _value = decodeURIComponent(value);
+      switch (typeof result[key]) {
+        case 'undefined':
+          result[key] = _value;
+          break;
+        case 'string':
+          arr = [result[key], _value];
+          result[key] = arr;
+          break;
+        default:
+          result[key].push(_value);
       }
-    });
+    }
+  });
 
-    return result;
-  };
+  return result;
+};
 
-  if (typeof module !== 'undefined' && module.exports) {
-    module.exports = nx.qs;
-  }
-})();
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = nx.qs;
+}
